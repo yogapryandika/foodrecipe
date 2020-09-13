d176bdf629dd6e5d5693a9bec56cabaea2255174
Yoga Nugraha Pryandika <yogapryandika@gmail.com>
2020-09-13 23:40:56 +0700

    adding API recipe and search init

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,32 @@
-import num from './test';
+import Search from './models/Search';
 
-const x = 23;
-console.log(`I imported ${num} from another module called text.js and variable x is ${x}`);
+/* Global State
+ * - Search object
+ * - Current recipe object
+ * - Shopping recipes
+ * - Liked object
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1. get query from view
+  const query = 'pizza';
+
+  if (query) {
+    // 2. new search object and add to state
+    state.search = new Search(query);
+
+    // 3. prepare UI for results
+
+    // 4. search recipes
+    await state.search.getResults();
+
+    // 5. render result
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+  e.preventDefault();
+  controlSearch();
+});
